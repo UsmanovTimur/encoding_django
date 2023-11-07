0a610917923278fc05c41f889a28c67b4dac426d
Усманов Тимур <usmanofftimur@yandex.ru>
2023-11-07 23:13:31 +0500

    encrypt package

--- encrypt_nuitka2.sh
+++ encrypt_nuitka2.sh
@@ -1,2 +1,7 @@
 # Скрипт сборки модуля core
 nuitka3 --module core --include-package=core --remove-output --no-pyi-file --follow-import-to=core
+
+for f in $(find core/ -name "*.py")
+do
+ rm $f
+done
